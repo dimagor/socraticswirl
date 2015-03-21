be836354e1e47eef2720b2cfc4b928ee7aafd669
Dima Gorenshteyn <dima@nat-oitwireless-inside-vapornet100-c-30521.Princeton.EDU>
2015-03-21 14:07:13 -0400

    Relies on lessondb, better organization

--- inst/socraticswirlapp/server.R
+++ inst/socraticswirlapp/server.R
@@ -4,12 +4,19 @@ library(ggplot2)
 
 shinyServer(function(input, output, session) {
   
-  # Reactive Input
+  # Static Definitions ----------
+  getPctColor <- function(pct){
+    typeColors = c("black","red","orange","yellow","light-blue","navy","teal","aqua","lime","olive","green")
+    typeColors[round(as.numeric(pct) / 10) + 1]
+  }
+  lectureInfo <- Parse_retrieve("lecdb_dima")
+  
+  # Reactive Functions ---------------
   usersLogged <- reactive({
     input$refresh #Refresh when button is clicked
     interval <- max(as.numeric(input$interval), 5)
     if(input$interval != FALSE) invalidateLater(interval * 1000, session)
-    Parse_retrieve("udb_dima") %>% group_by()
+    Parse_retrieve("udb_dima") %>% .$student %>% unique %>% length
   })  
   
   questionsAnswered <- reactive({
@@ -24,6 +31,35 @@ shinyServer(function(input, output, session) {
     Sys.time()
   })
   
+  selectedExercise <- reactive({
+    questionsAnswered() %>% filter(exercise == input$exerciseID)
+  })
+  
+  # Header --------
+  
+  output$progressMenu <- renderMenu({
+    progress_breakdown <- questionsAnswered() %>%
+      group_by(exercise) %>%
+      distinct(student, exercise, correct) %>%
+      summarise(n=sum(correct)) %>% 
+      mutate(pct=round(n / usersLogged(), 2)*100)
+    progress_breakdown <- left_join(lectureInfo,progress_breakdown, by="exercise") %>%
+      mutate(pct=ifelse(is.na(pct),0,pct))
+    progress_msgs <- apply(progress_breakdown, 1, function(row) {
+      taskItem(value = row[["pct"]], 
+               color = getPctColor(row[["pct"]]),
+               paste("Exercise:", row[["exercise"]])
+      )
+    })
+    
+    dropdownMenu(type = "tasks", .list = progress_msgs)
+  })
+  
+  # Sidebar --------------
+  output$usersessions <- renderUI({
+    h3("Sessions:", as.character(usersLogged()))
+  })
+  
   output$timeSinceLastUpdate <- renderUI({
     # Trigger this every 5 seconds
     invalidateLater(5000, session)
@@ -35,27 +71,29 @@ shinyServer(function(input, output, session) {
     )
   })
   
-  typeColors = c("black","red","orange","yellow","light-blue","navy","teal","aqua","lime","olive","green")
   
-  output$progressMenu <- renderMenu({
-    progress_breakdown <- questionsAnswered() %>%
-      group_by(exercise) %>%
-      distinct(student,exercise,correct) %>%
-      summarise(n=sum(correct)) %>% 
-      mutate(pct=round(n/sessions,2)*100)
-    progress_msgs <- apply(progress_breakdown, 1, function(row) {
-      taskItem(value = row[["pct"]], color = typeColors[round(as.numeric(row[["pct"]])/10)+1], paste("Exercise:",row[["exercise"]]))
-    })
-    
-    dropdownMenu(type = "tasks", .list = progress_msgs)
+  # BODY ------------
+  output$selectExercise <- renderUI({
+    exercises = as.list(lectureInfo$exercise)
+    selectInput("exerciseID", "Select Exercise:", exercises, selected = "1")
   })
   
-  output$sessions <- renderUI({
-      udb <- usersLogged()
-      users <- length(unique(udb$student))
-      h3("Sessions:",users
-      )
-    })
+  output$attemptedBox <- renderValueBox({
+    attempted = selectedExercise() %>% distinct(student) %>% nrow
+    attempted_pct = round(attempted/usersLogged() * 100)
+    valueBox(
+      paste0(as.character(attempted_pct), "%"), "Attempted", icon = icon("bars"),
+      color = getPctColor(attempted_pct)
+    )
+  })
+  output$completedBox <- renderValueBox({
+    completed = selectedExercise() %>% filter(correct) %>% distinct(student) %>% nrow
+    completed_pct = round(completed/usersLogged() * 100)
+    valueBox(
+      paste0(as.character(completed_pct), "%"), "Progress", icon = icon("thumbs-up", lib = "glyphicon"),
+      color = getPctColor(completed_pct)
+    )
+  })
   
  
   
--- inst/socraticswirlapp/ui.R
+++ inst/socraticswirlapp/ui.R
@@ -1,44 +1,52 @@
 library(shiny)
 library(shinydashboard)
 
-dashboardPage(
-  dashboardHeader(title = "SocraticSwirl",
-                  dropdownMenuOutput("progressMenu")),
-  dashboardSidebar(
-    box(width = NULL,
-        uiOutput("sessions")),
-    box(width = NULL,
-        selectInput("interval", "Refresh interval",
-                    choices = c(
-                      "5 seconds" = 5,
-                      "15 seconds" = 15,
-                      "30 seconds" = 30,
-                      "1 minute" = 50,
-                      "5 minutes" = 600,
-                      "Off" = FALSE
-                    ),
-                    selected = "30"
-        ),
-        uiOutput("timeSinceLastUpdate"),
-        actionButton("refresh", "Refresh now"))
-    ),
-  dashboardBody()
+header <- dashboardHeader(title = "SocraticSwirl",
+                          dropdownMenuOutput("progressMenu"))
+
+sidebar <- dashboardSidebar(
+  box(width = NULL,
+      uiOutput("usersessions")),
+  
+  sidebarMenu(
+    menuItem("Exercise Dashboard", tabName = "exercise"),
+    menuItem("Lesson Overview", tabName = "overview", icon = icon("dashboard"))
+  ),
+  
+  p(), #Fix for better separation
+  
+  box(
+    width = NULL, title = "Controls", collapsible = TRUE,
+    selectInput("interval", "Refresh interval",
+                choices = c(
+                  "5 seconds" = 5,
+                  "15 seconds" = 15,
+                  "30 seconds" = 30,
+                  "1 minute" = 50,
+                  "5 minutes" = 600,
+                  "Off" = FALSE),
+                selected = "30"),
+    uiOutput("timeSinceLastUpdate"),
+    actionButton("refresh", "Refresh now")
+  )
 )
 
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "exercise",
+            fluidRow(
+                     box(uiOutput("selectExercise")),
+                     valueBoxOutput("attemptedBox"),
+                     valueBoxOutput("completedBox")
+                     # % attempted, % complete
+              )
+            #Plot switch, Table of answers
+            ),
+    tabItem(tabName = "overview",
+            h2("Dashboard tab content")
+    )
+    )
+  #QA Tab: List questions and add button to mark resolved
+)
 
-
-# 
-# shinyUI(fluidPage(
-#   fluidRow(
-#     column(1,
-#             selectInput(inputId = "exercise",
-#               label = "Exercise Number:",
-#               choices = 10:11,
-#               selected = 11)
-#            ),
-#     column(2,verbatimTextOutput(outputId = "sessions_started"))
-#     ),
-#   hr()
-#   # dataTableOutput(outputId = "incorrect_answers")
-#   # plotOutput(outputId = "bar_plot", height = "300px")
-# ))
+dashboardPage(header,  sidebar,  body)
