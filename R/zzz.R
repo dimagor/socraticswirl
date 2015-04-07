59bf74b91b56556155c9c463794b246a4b6c96ea
dgrtwo <dgrtwo@princeton.edu>
2015-04-07 05:40:00 -0400

    Changed package startup message to be specific to Socratic Swirl. Closes #15

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,10 @@
 .onAttach <- function(...) {
-  if(length(ls(envir=globalenv())) > 0) {
-    packageStartupMessage(
-      make_pretty("Hi! I see that you have some variables saved in your",
-      "workspace. To keep things running smoothly, I recommend you clean up",
-      "before starting swirl.", skip_after=TRUE),
-      make_pretty("Type ls() to see a list of the variables in your workspace.",
-      "Then, type rm(list=ls()) to clear your workspace.", skip_after=TRUE),
-      make_pretty("Type swirl() when you are ready to begin.", skip_after=TRUE)
-    )
-  } else {
-    packageStartupMessage(
-      make_pretty("Hi! Type swirl() when you are ready to begin.",
+  packageStartupMessage(
+      make_pretty("Hi! When you're ready, type socratic_swirl(...) ",
+                  "with the options your instructor gives you to ",
+                  "specify your lesson and instructor.",
                   skip_after=TRUE)
     )
-  }
   
   # set socratic swirl options
   Sys.setenv(PARSE_APPLICATION_ID = "C0pM75Sepnt5WhK6P6yhRA0TqVa6Xa3vqwZjpLfT",
