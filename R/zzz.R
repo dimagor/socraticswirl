5d1d8a715c18bc02fc342d62add8e6cb8240205c
dgrtwo <dgrtwo@princeton.edu>
2015-04-06 20:01:25 -0400

    Set application ID and key in .onAttach. Closes #13.

--- R/zzz.R
+++ R/zzz.R
@@ -14,6 +14,11 @@
                   skip_after=TRUE)
     )
   }
+  
+  # set socratic swirl options
+  Sys.setenv(PARSE_APPLICATION_ID = "C0pM75Sepnt5WhK6P6yhRA0TqVa6Xa3vqwZjpLfT",
+             PARSE_API_KEY = "HyXS1gEn6gf7gibjDJVWPYsnIoc0SXcp4mwohdmI")
+  
   invisible()
 }
 
